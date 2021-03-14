61049a12f741568138206ba996188aa90ac189d9
Reuben T Allik <reubenallik@gmail.com>
2021-03-14 17:55:14 -0400

    added random titles

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,28 @@
 import React, { useState } from 'react';
+import {titles} from './../../99-extras/titles.js';
+
+const getTitle = () => {
+  return Math.floor(Math.random()*21);
+}
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState('hello world'))
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler);
+
+  const [text, setText] = useState('random title')
+  const handleClick = () => {
+    const newtitle = titles[getTitle()];
+    console.log(getTitle())
+    setText(newtitle)
+  }
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className='btn' onClick={handleClick}>Change Title</button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
---
+++ src/tutorial/99-extras/titles.js
@@ -0,0 +1,25 @@
+const titles = ['The Bible', 
+                'Don Quixote', 
+                'A Tale of Two Cities', 
+                'The Lord of the Rings', 
+                'The Little Prince', 
+                'Harry Potter and the Sorcerers Stone',
+                'And Then There Were None',
+                'The Dream of The Red Chamber',
+                'She: A History of Adventure',
+                'The Lion, the Witch and the Wardrobe',
+                'The Da Vinci Code',
+                'Think and Grow Rich',
+                'Harry Potter and the Chamber of Secrets',
+                'Harry Potter and the Prisoner of Azkaban',
+                'Harry Potter and the Goblet of Fire',
+                'Harry Potter and the Order of the Pheonix',
+                'Harry Potter and the Deathly Hallows',
+                'One Hundred Years of Solitude',
+                'Lolita',
+                'Anne of Green Gables',
+                'Charlottes Web',
+                'Black Beauty'
+            ];
+
+export {titles};
